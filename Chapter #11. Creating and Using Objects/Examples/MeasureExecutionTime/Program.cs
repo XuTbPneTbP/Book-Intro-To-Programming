052e771d490173a6f4218b95f0a64996787b9082
baiganio <big_l@abv.bg>
2016-05-03 22:06:38 +0300

    Environment.TickCount Example to measure code execution time.

---
+++ Chapter #11. Creating and Using Objects/Examples/MeasureExecutionTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasureExecutionTime
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sum = 0;
+            int startTime = Environment.TickCount;
+
+            /* Code fragment to be tested. Returned result is in miliseconds. */
+            for (long i = 0; i < 100000000; i++)
+            {
+                sum++;
+            }
+            
+            int endTime = Environment.TickCount;
+            var elapsedTime = (endTime - startTime) / 1000.0;
+            Console.WriteLine($"Time elapsed : {elapsedTime} sec.");
+        }
+    }
+}
